04b27a9878433857e10d989a60ed2bfc57a2b2ef
jean bosco yao <jeanboscoyao1@gmail.com>
2021-12-09 23:59:17 +0000

    j'ai essaye de modifier ton progrmme avev beaucoup d'incoherence fait moi un retour

--- vector.py
+++ vector.py
@@ -1,9 +1,17 @@
 import math
 
-class vector:
+class Vector:
     def __init__(self,x,y):
         self.x = x
         self.y = y
     def norm(self):
         result = math.sqrt(self.x**2+self.y**2)
         return result
+    def parite(self):
+        est_paire=False
+        if norm() % 2==0:
+            est_paire=True
+
+v1=Vector(4,-6)
+print(v1.norm())
+print(v1.parite())
