327f9037527b7f878a54dec9d5e0540c6e59125c
Romeo tohoun <romeotohoun@gmail.com>
2021-12-09 19:29:40 +0000

    Ajout d'une methode qui calcule la norme

--- vector.py
+++ vector.py
@@ -1,4 +1,9 @@
+import math
+
 class vector:
     def __init__(self,x,y):
         self.x = x
         self.y = y
+    def norm(self):
+        result = math.sqrt(self.x**2+self.y**2)
+        return result
